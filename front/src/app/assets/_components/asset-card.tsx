601f196434a51474b70eb74978e27ed77341b5c6
fs1g17 <fs1g17@soton.ac.uk>
2025-11-30 16:01:15 +0400

    feat: make cards clickable

--- front/src/app/assets/_components/asset-card.tsx
+++ front/src/app/assets/_components/asset-card.tsx
@@ -1,17 +1,20 @@
 import { Asset } from "@/types/asset";
 import AssetCardCreator from "./asset-card-creator";
+import Link from "next/link";
 
 export default function AssetCard({ asset }: { asset: Asset }) {
   return (
-    <div
-      className="w-full max-w-sm aspect-[435/631] bg-cover bg-center rounded-lg p-2 flex flex-col justify-between"
-      style={{ backgroundImage: `url(${asset.thumbnailUrl})` }}
-    >
-      <div>hello</div>
-      <AssetCardCreator
-        creator={asset.creator}
-        deliverable={asset.deliverable}
-      />
-    </div>
+    <Link href={`/assets/${asset.id}`}>
+      <div
+        className="w-full max-w-sm aspect-[435/631] bg-cover bg-center rounded-lg p-2 flex flex-col justify-between"
+        style={{ backgroundImage: `url(${asset.thumbnailUrl})` }}
+      >
+        <div>hello</div>
+        <AssetCardCreator
+          creator={asset.creator}
+          deliverable={asset.deliverable}
+        />
+      </div>
+    </Link>
   );
 }
